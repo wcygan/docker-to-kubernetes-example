591094aee0318fab0747ef1fda7f694492cc54f3
Will Cygan <wcygan.io@gmail.com>
2024-01-16 09:45:09 -0600

    Added some scripts for generating & removing protobuf files

---
+++ scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the name of the current directory
+current_directory=$(basename "$(pwd)")
+
+# Check if the current directory is not "docker-to-kubernetes-example"
+if [ "$current_directory" != "docker-to-kubernetes-example" ]; then
+    echo "Please run this from the root directory of the project (docker-to-kubernetes-example)."
+    exit 1
+fi
+
+echo "Cleaning generated files from protobuf..."
+rm -rf generated
